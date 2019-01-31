d32db56a1837a3486b4a2b125aa027debe659cb1
Impey <T.s.impey@gmail.com>
2019-01-31 13:10:47 +0000

    Update Currentweather.rb

--- lib/services/Currentweather.rb
+++ lib/services/Currentweather.rb
@@ -9,7 +9,7 @@ class Currentweather
   base_uri 'https://api.openweathermap.org'
   
   def getdata
-    JSON.parse(self.class.get("/data/2.5/weather?q=London,uk&appid=84fd4dd594e65a1240f547b1a0a90188").body)
+    JSON.parse(self.class.get("/data/2.5/weather?q=London,uk&).body)
   
   end
 
